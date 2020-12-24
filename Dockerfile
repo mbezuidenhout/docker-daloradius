f4f30b63165335610630c0a95fe77f84a14d0d76
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2020-12-24 12:35:37 +0200

    Fixed entrypoint script and added GD

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,14 @@ LABEL maintainer="Marius Bezuidenhout <marius.bezuidenhout@gmail.com>"
 
 RUN apt-get update \
  && apt-get install --no-install-recommends --assume-yes --quiet \
-        ca-certificates openssl \
+        ca-certificates openssl libpng-dev \
  && apt-get clean \
  && rm -rf /var/lib/apt/lists/* \
  && ldconfig
 
 RUN update-ca-certificates -f \
+ && docker-php-ext-install mysqli \
+ && docker-php-ext-install db \
  && mkdir -p /tmp/pear/cache \
  && curl -O https://pear.php.net/go-pear.phar \
  && echo | php go-pear.phar \
@@ -27,9 +29,11 @@ ENV DALO_VERSION 1.1-3
 RUN curl -LO https://github.com/lirantal/daloradius/archive/"$DALO_VERSION".tar.gz \
  && tar -xzf "$DALO_VERSION".tar.gz \
  && rm "$DALO_VERSION".tar.gz \
- && mv daloradius-"$DALO_VERSION" /var/www/html/daloradius \
- && chown -R www-data:www-data /var/www/html/daloradius \
- && chmod 644 /var/www/html/daloradius/library/daloradius.conf.php
+ && mv daloradius-"$DALO_VERSION" /var/www/daloradius \
+ && chown -R www-data:www-data /var/www/daloradius \
+ && chmod 644 /var/www/daloradius/library/daloradius.conf.php
+
+WORKDIR /var/www/daloradius
 
 COPY docker-entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,8 +24,20 @@ file_env() {
 }
 
 if [[ "$1" == apache2* ]]; then
-    : ${HTTPS_ENABLED:=false}
     hostname="${APACHE_HOSTNAME:-localhost}"
+    a2dissite 000-default
+    cat > /etc/apache2/sites-available/${hostname}.conf <<EOL
+    <VirtualHost *:80>
+        ServerName ${hostname}
+        ServerAdmin webmaster@localhost
+	    DocumentRoot /var/www/daloradius
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+	    CustomLog \${APACHE_LOG_DIR}/access.log combined
+    </VirtualHost>
+EOL
+    a2ensite ${hostname}
+
+    : ${HTTPS_ENABLED:=false}
     if [[ $HTTPS_ENABLED != "false" ]]; then
         if [ ! -e /etc/apache2/ssl/${hostname}.crt ] || [ ! -e /etc/apache2/ssl/${hostname}.key ]; then
             # if the certificates don't exist then make them
@@ -40,7 +52,7 @@ if [[ "$1" == apache2* ]]; then
         <IfModule mod_ssl.c>
             <VirtualHost *:443>
                 ServerName ${hostname}
-                DocumentRoot /var/www/html
+                DocumentRoot /var/www/daloradius
                 ErrorLog \${APACHE_LOG_DIR}/error.log
                 CustomLog \${APACHE_LOG_DIR}/access.log combined
                 SSLEngine on
@@ -52,6 +64,63 @@ EOL
         a2enmod ssl
         a2ensite ${hostname}-ssl
     fi
+fi
+
+envs=(
+    RADIUS_DB_HOST
+    RADIUS_DB_USER
+    RADIUS_DB_PASSWORD
+    RADIUS_DB_NAME
+    RADIUS_DB_PORT
+)
+haveConfig=
+for e in "${envs[@]}"; do
+    file_env "$e"
+    if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
+        haveConfig=1
+    fi
+done
+
+if [ "$haveConfig" ]; then
+    : "${RADIUS_DB_HOST:=mysql}"
+    : "${RADIUS_DB_USER:=root}"
+    : "${RADIUS_DB_PASSWORD:=}"
+    : "${RADIUS_DB_NAME:=freeradius}"
+    : "${RADIUS_DB_PORT:=3306}"
+
+    # Ensure that line ends are unix
+    sed -ri -e 's/\r$//' /var/www/daloradius/library/daloradius.conf.php
+
+    sed_escape_lhs() {
+        echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
+    }
+    sed_escape_rhs() {
+        echo "$@" | sed -e 's/[\/&]/\\&/g'
+    }
+    php_escape() {
+        local escaped="$(php -r 'var_export(('"$2"') $argv[1]);' -- "$1")"
+        if [ "$2" = 'string' ] && [ "${escaped:0:1}" = "'" ]; then
+            escaped="${escaped//$'\n'/"' + \"\\n\" + '"}"
+        fi
+        echo "$escaped"
+    }
+    set_config() {
+        key="$1"
+        value="$2"
+        var_type="${3:-string}"
+        start="\[(['\\\"])$(sed_escape_lhs "$key")\2\]\s*="
+        end=";"
+        if [ "${key:0:1}" = '$' ]; then
+            start="^(\s*)$(sed_escape_lhs "$key")\s*="
+            end=";"
+        fi
+        sed -ri -e "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" /var/www/daloradius/library/daloradius.conf.php
+    }
+    set_config 'CONFIG_DB_HOST' "$RADIUS_DB_HOST"
+    set_config 'CONFIG_DB_USER' "$RADIUS_DB_USER"
+    set_config 'CONFIG_DB_PASS' "$RADIUS_DB_PASSWORD"
+    set_config 'CONFIG_DB_NAME' "$RADIUS_DB_NAME"
+    set_config 'CONFIG_DB_PORT' "$RADIUS_DB_PORT"
 fi
 
 # first arg is `-f` or `--some-option`
